28edc0de24ab4095e54a23e15270a8109dddee51
pegasus614 <emmanuelodeh5555@gmail.com>
2025-10-21 21:11:37 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Toggle navigation menu on mobile
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('active');
+});
